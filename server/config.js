6aa3e533399f197c1c82c210d80f98a0c9419987
laibao101 <369632567@qq.com>
2018-04-25 23:47:24 +0800

    init

---
+++ server/config.js
@@ -0,0 +1,6 @@
+export const qiniuConfig = {
+    "bucket": "graduate",
+    "pic" : "pic.webtotal.cn",
+    "AK":"7Tk6wvF4zl62DVY1vutT5iyk3C019H82xfHZrut",
+    "SK":"BSgd3oWNAtMdg3LwQJbBQOvCesdZsexTo3YJ81WQ"
+};
