d69df115d5d03331be36a47110b144c70c05a64e
unknown <zhouhua@dev-wh.douyu.tv>
2018-04-29 13:55:53 +0800

    评论

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -15,16 +15,6 @@ function postComments(state=[],action){
 				}
 			];
 			break;
-		case 'REMOVE_COMMENT':
-			//we need to return the new state without the deleted commnet
-			return [
-				//from the start to the one we want to delete
-				...state.slice(0,action.i),
-				//after the deleted one ,to the end
-				...state.slice(action.i+1)
-			];
-			console.log('remove comment');
-			break;
 		default:
 			return state;
 	}
